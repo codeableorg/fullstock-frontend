85eda68e5575e9db4ee4500de456098cd728aa22
Diego Torres <diegotc86@gmail.com>
2024-12-20 10:25:32 -0500

    feat: update Home component with hero section and container; replace clsx with cn utility

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,7 +1,28 @@
+import { Button } from "@/components/ui/button";
+import { Container } from "@/components/ui/container";
+import heroSrc from "@/assets/hero.jpeg";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <section
+        className="text-center bg-cover bg-no-repeat bg-center"
+        style={{
+          backgroundImage: `linear-gradient(0deg, rgba(0, 0, 0, 0.5) 0%, rgba(0, 0, 0, 0.5) 100%), url('${heroSrc}')`,
+        }}
+      >
+        <Container className="py-32 max-w-3xl">
+          <h2 className="text-4xl font-bold mb-4 md:text-6xl">
+            Nuevos productos disponibles
+          </h2>
+          <p className="text-xl mb-8">
+            Un pequeño lote de increíbles productos acaba de llegar.
+            <br />
+            Agrega tus favoritos al carrito antes que se agoten.
+          </p>
+          <Button size="xl">Compra ahora</Button>
+        </Container>
+      </section>
+    </>
   );
 }
