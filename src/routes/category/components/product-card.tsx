3dc096a3b6d09056df6bf190796e2933f6ea0074
Diego Torres <diegotc86@gmail.com>
2025-01-23 12:26:18 -0500

    refactor: enhance ProductCard layout with flexbox for improved responsiveness

--- src/routes/category/components/product-card.tsx
+++ src/routes/category/components/product-card.tsx
@@ -2,7 +2,7 @@ import { Product } from "@/services/products/product.types";
 
 export function ProductCard({ product }: { product: Product }) {
   return (
-    <div className="border border-separator rounded-xl relative overflow-hidden">
+    <div className="border border-separator rounded-xl relative overflow-hidden flex flex-col">
       <div className="object-cover aspect-[3/4] bg-muted ">
         <img
           src={product.imgSrc}
@@ -10,10 +10,10 @@ export function ProductCard({ product }: { product: Product }) {
           className="object-cover w-full h-full"
         />
       </div>
-      <div className="p-4 flex flex-col gap-2">
+      <div className="p-4 flex flex-col gap-2 grow">
         <h2 className="text-sm font-medium">{product.title}</h2>
         <p className="text-sm text-muted-foreground">{product.description}</p>
-        <p className="text-base font-medium">${product.price}</p>
+        <p className="text-base font-medium mt-auto">${product.price}</p>
       </div>
       {product.isOnSale && (
         <span className="absolute top-0 right-0 bg-primary text-primary-foreground px-2 py-1 text-sm font-medium rounded-es-xl">
