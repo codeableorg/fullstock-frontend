217179eece4eae0e3e53ee76a0c4f18d91526369
Diego Torres <diegotc86@gmail.com>
2025-01-23 12:35:34 -0500

    feat: wrap ProductCard in Link component for improved navigation

--- src/routes/category/components/product-card.tsx
+++ src/routes/category/components/product-card.tsx
@@ -1,25 +1,28 @@
 import { Product } from "@/services/products/product.types";
+import { Link } from "react-router";
 
 export function ProductCard({ product }: { product: Product }) {
   return (
-    <div className="border border-separator rounded-xl relative overflow-hidden flex flex-col">
-      <div className="object-cover aspect-[3/4] bg-muted ">
-        <img
-          src={product.imgSrc}
-          alt={product.title}
-          className="object-cover w-full h-full"
-        />
+    <Link to={`/products/${product.id}`} className="block group">
+      <div className="border border-separator rounded-xl relative overflow-hidden flex flex-col">
+        <div className="object-cover aspect-[3/4] bg-muted ">
+          <img
+            src={product.imgSrc}
+            alt={product.title}
+            className="object-cover w-full h-full group-hover:scale-105 transition-transform"
+          />
+        </div>
+        <div className="p-4 flex flex-col gap-2 grow">
+          <h2 className="text-sm font-medium">{product.title}</h2>
+          <p className="text-sm text-muted-foreground">{product.description}</p>
+          <p className="text-base font-medium mt-auto">${product.price}</p>
+        </div>
+        {product.isOnSale && (
+          <span className="absolute top-0 right-0 bg-primary text-primary-foreground px-2 py-1 text-sm font-medium rounded-es-xl">
+            🚀 Oferta
+          </span>
+        )}
       </div>
-      <div className="p-4 flex flex-col gap-2 grow">
-        <h2 className="text-sm font-medium">{product.title}</h2>
-        <p className="text-sm text-muted-foreground">{product.description}</p>
-        <p className="text-base font-medium mt-auto">${product.price}</p>
-      </div>
-      {product.isOnSale && (
-        <span className="absolute top-0 right-0 bg-primary text-primary-foreground px-2 py-1 text-sm font-medium rounded-es-xl">
-          🚀 Oferta
-        </span>
-      )}
-    </div>
+    </Link>
   );
 }
