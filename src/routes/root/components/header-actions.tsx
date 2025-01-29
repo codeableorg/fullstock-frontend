f7e0200ce02e9eeeac0b26f496e9215743a2ee13
Diego Torres <diegotc86@gmail.com>
2025-01-29 16:32:30 -0500

    feat: display total items in cart button

--- src/routes/root/components/header-actions.tsx
+++ src/routes/root/components/header-actions.tsx
@@ -3,8 +3,14 @@ import { Search, ShoppingCart, User2 } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
+import { useCart } from "@/providers/cart";
 
 export default function HeaderActions() {
+  const {
+    state: { items },
+  } = useCart();
+  const totalItems = items?.reduce((sum, item) => sum + item.quantity, 0) || 0;
+
   return (
     <div className="flex gap-2 items-center">
       <Button size="xl-icon" variant="ghost" aria-label="Buscar">
@@ -19,9 +25,15 @@ export default function HeaderActions() {
         variant="ghost"
         aria-label="Carrito de compras"
         asChild
+        className="relative"
       >
         <Link to="/cart">
           <ShoppingCart />
+          {totalItems > 0 && (
+            <span className="absolute -bottom-1 -right-1 bg-primary text-primary-foreground text-xs rounded-full h-5 w-5 flex items-center justify-center">
+              {totalItems}
+            </span>
+          )}
         </Link>
       </Button>
     </div>
