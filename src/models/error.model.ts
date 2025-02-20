767badb935b73cb7b72ca70d4f7f6ddde8bab2e7
Diego Torres <diegotc86@gmail.com>
2025-02-19 20:31:23 -0500

    feat: enhance error handling in category service and modify ApiError interface

--- src/models/error.model.ts
+++ src/models/error.model.ts
@@ -1,6 +1,12 @@
-export interface ApiErrorResponse {
+export interface ApiError {
   error: {
     message: string;
     code: string;
   };
+}
+
+export function isApiError(data: any): data is ApiError {
+  return (
+    "error" in data && typeof data.error === "object" && "message" in data.error
+  );
 }
--- src/services/category.service.ts
+++ src/services/category.service.ts
@@ -1,50 +1,41 @@
 import { API_URL } from "@/config";
 import { Category } from "@/models/category.model";
-import { ApiErrorResponse } from "@/models/error.model";
+import { isApiError } from "@/models/error.model";
 
 export async function getAllCategories(): Promise<Category[]> {
-  const response = await fetch(API_URL + "/categories");
-
-  let data;
-
   try {
-    data = await response.json();
-  } catch (error) {
-    console.error(error);
-    throw new Error("Error parsing JSON response");
-  }
+    const response = await fetch(API_URL + "/categories");
 
-  if (!response.ok) {
-    const errorMessage =
-      (data as ApiErrorResponse)?.error?.message || "Unknown error";
-    const error = new Error(errorMessage);
+    const data = await response.json();
+
+    if (!response.ok) {
+      if (isApiError(data)) throw new Error(data.error.message);
+      throw new Error("Unknown error");
+    }
+
+    return data as Category[];
+  } catch (error) {
     console.error(error);
     throw error;
   }
-
-  return data as Category[];
 }
 
-export async function getCategoryBySlug(slug: string): Promise<Category | null> {
-  const response = await fetch(API_URL + "/categories/" + slug);
-
-  let data;
-
+export async function getCategoryBySlug(
+  slug: string
+): Promise<Category | null> {
   try {
-    data = await response.json();
-    } catch (error) {
-      console.error(error);
-      throw new Error("Error parsing JSON response");
-    }
+    const response = await fetch(API_URL + "/categories/" + slug);
+
+    const data = await response.json();
 
     if (!response.ok) {
-      const errorMessage =
-        (data as ApiErrorResponse)?.error?.message || "Unknown error";
-      const error = new Error(errorMessage);
-      console.error(error);
-      throw error;
+      if (isApiError(data)) throw new Error(data.error.message);
+      throw new Error("Unknown error");
     }
-  
-    return data as Category;
 
+    return data as Category;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
