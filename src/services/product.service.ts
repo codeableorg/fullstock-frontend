3bfa6fd64a570a3b17aebeade2fa44d307d80f1e
mike <mike.vera.q@gmail.com>
2025-02-23 04:19:25 -0500

    feat: update getProductsByCategorySlug to fetch products from API and handle errors

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -3,19 +3,29 @@ import { Category } from "@/models/category.model";
 import { isApiError } from "@/models/error.model";
 import { Product } from "@/models/product.model";
 
-import { products } from "../fixtures/products.fixture";
+export async function getProductsByCategorySlug(categorySlug: Category["slug"]): Promise<Product[]> {
+  try {
+    const res_category = await fetch(`${API_URL}/categories/${categorySlug}`);
+    const category = await res_category.json();
 
-export function getProductsByCategorySlug(
-  categorySlug: Category["slug"]
-): Promise<Product[]> {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      const filteredProducts = products.filter(
-        (product) => product.categorySlug === categorySlug
-      );
-      resolve(filteredProducts);
-    }, 500);
-  });
+    if (!res_category.ok) {
+      if (isApiError(category)) throw new Error(category.error.message);
+      throw new Error("Unknown error");
+    }
+
+    const res_products = await fetch(`${API_URL}/products?categoryId=${category.id}`);
+    const products = await res_products.json();
+
+    if (!res_products.ok) {
+      if (isApiError(products)) throw new Error(products.error.message);
+      throw new Error("Unknown error");
+    }
+
+    return products as Product[];
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
 
 export async function getProductById(id: number): Promise<Product> {
