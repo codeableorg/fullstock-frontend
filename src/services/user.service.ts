408e65cc4d566cf016f6e9bfbbf33ba104dc771b
Angel Briones Cerquín <angelb.c@hotmail.com>
2025-03-07 21:22:42 +0000

    refactor: remove old functions before integrating client function

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,31 +1,5 @@
-// import { API_URL } from "@/config";
 import { client, getToken, setToken } from "@/lib/utils";
-// import { isApiError } from "@/models/error.model";
 import { AuthResponse, User } from "@/models/user.model";
-
-// export async function findOrCreateGuestUser(email: string): Promise<void> {
-//   try {
-//     const response = await fetch(API_URL + "/auth/newOrExist", {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({ email: email }),
-//     });
-
-//     const data = (await response.json()) as AuthResponse;
-
-//     if (data.token.length > 0) setToken(data.token);
-
-//     if (!response.ok) {
-//       if (isApiError(data)) throw new Error(data.error.message);
-//       throw new Error("Unknown error");
-//     }
-//   } catch (error) {
-//     console.error(error);
-//     throw error;
-//   }
-// }
 
 export async function findOrCreateGuestUser(email: string): Promise<void> {
   const body = await client<AuthResponse>("/auth/newOrExist", {
@@ -36,37 +10,6 @@ export async function findOrCreateGuestUser(email: string): Promise<void> {
     setToken(body.token);
   }
 }
-
-// export async function updateUser(
-//   updatedUser: Partial<User>
-// ): Promise<AuthResponse["user"]> {
-//   try {
-//     const token = getToken();
-
-//     const response = await fetch(API_URL + "/users/me", {
-//       method: "PATCH",
-//       headers: {
-//         "Content-Type": "application/json",
-//         Authorization: `Bearer ${token}`,
-//       },
-//       body: JSON.stringify({ updatedUser }),
-//     });
-
-//     const data = (await response.json()) as AuthResponse;
-
-//     if (!response.ok) {
-//       if (isApiError(data)) throw new Error(data.error.message);
-//       throw new Error("Unknown error");
-//     }
-
-//     setToken(data.token);
-
-//     return data.user;
-//   } catch (error) {
-//     console.error(error);
-//     throw error;
-//   }
-// }
 
 export async function updateUser(
   updatedUser: Partial<User>
