bb6f1ad13f1cebf49347b4092f7e9d106200f03f
Jeffcar1993 <jphoenix.07@hotmail.com>
2025-03-07 12:15:47 -0500

    feat: refactor category service to use unified API client for fetching categories

--- src/services/category.service.ts
+++ src/services/category.service.ts
@@ -1,41 +1,13 @@
-import { API_URL } from "@/config";
+import { client } from "@/lib/utils";
 import { Category } from "@/models/category.model";
-import { isApiError } from "@/models/error.model";
+
 
 export async function getAllCategories(): Promise<Category[]> {
-  try {
-    const response = await fetch(API_URL + "/categories");
-
-    const data = await response.json();
-
-    if (!response.ok) {
-      if (isApiError(data)) throw new Error(data.error.message);
-      throw new Error("Unknown error");
-    }
-
-    return data as Category[];
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
+  return client<Category[]>("/categories");
 }
 
 export async function getCategoryBySlug(
   slug: string
 ): Promise<Category | null> {
-  try {
-    const response = await fetch(API_URL + "/categories/" + slug);
-
-    const data = await response.json();
-
-    if (!response.ok) {
-      if (isApiError(data)) throw new Error(data.error.message);
-      throw new Error("Unknown error");
-    }
-
-    return data as Category;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
+  return client<Category>(`/categories/${slug}`);
 }
