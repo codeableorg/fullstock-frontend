f12923d1b6b9fd3a69e19a6a6b48049b19fa7d97
Diego Torres <diegotc86@gmail.com>
2025-03-07 19:43:51 -0500

    fix: removing error handling on order.service

--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -11,25 +11,13 @@ export async function createOrder(
     formData
   ) as unknown as OrderDetails;
 
-  try {
-    const data = await client<{ id: string }>(`${API_URL}/orders`, {
-      body: { items, shippingDetails },
-    });
+  const data = await client<{ id: string }>(`${API_URL}/orders`, {
+    body: { items, shippingDetails },
+  });
 
-    return { orderId: data.id };
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
+  return { orderId: data.id };
 }
 
 export async function getOrdersByUser(): Promise<Order[]> {
-  try {
-    const data = await client<Order[]>(`${API_URL}/orders`);
-
-    return data;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
+  return client<Order[]>(`${API_URL}/orders`);
 }
