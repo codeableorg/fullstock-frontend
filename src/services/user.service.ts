f6dcc5405f46c608dd1417f9f8c807af56fe5129
Diego Torres <diegotc86@gmail.com>
2025-03-07 19:51:49 -0500

    fix: remove unnecesary code on user.service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,9 +1,9 @@
-import { client, getToken, setToken } from "@/lib/utils";
+import { client, setToken } from "@/lib/utils";
 import { AuthResponse, User } from "@/models/user.model";
 
 export async function findOrCreateGuestUser(email: string): Promise<void> {
   const body = await client<AuthResponse>("/auth/newOrExist", {
-    body: { email: email },
+    body: { email },
   });
 
   if (body.token.length > 0) {
@@ -14,13 +14,8 @@ export async function findOrCreateGuestUser(email: string): Promise<void> {
 export async function updateUser(
   updatedUser: Partial<User>
 ): Promise<AuthResponse["user"]> {
-  const token = getToken();
-
   const body = await client<AuthResponse>("/users/me", {
     body: { updatedUser },
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
     method: "PATCH",
   });
 
