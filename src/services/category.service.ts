b4c49c989beb7f5e1c8a2f142a58e64d9b642411
Diego Torres <diegotc86@gmail.com>
2025-03-07 19:29:09 -0500

    fix: update getCategoryBySlug function signature

--- src/services/category.service.ts
+++ src/services/category.service.ts
@@ -1,13 +1,10 @@
 import { client } from "@/lib/utils";
 import { Category } from "@/models/category.model";
 
-
 export async function getAllCategories(): Promise<Category[]> {
   return client<Category[]>("/categories");
 }
 
-export async function getCategoryBySlug(
-  slug: string
-): Promise<Category | null> {
+export async function getCategoryBySlug(slug: string): Promise<Category> {
   return client<Category>(`/categories/${slug}`);
 }
