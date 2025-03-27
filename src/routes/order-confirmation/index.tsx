bc3482923208e55bef948882a4ea2c13f476952d
Kelly Arias <kellyarias993@gmail.com>
2025-03-26 20:16:19 -0500

    Migración de la ruta order-confirmation a Tailwind

--- src/routes/order-confirmation/index.tsx
+++ src/routes/order-confirmation/index.tsx
@@ -1,28 +1,25 @@
 import { useParams } from "react-router";
-
 import { Container } from "@/components/ui";
-
-import styles from "./styles.module.css";
 
 export default function OrderConfirmation() {
   const { orderId } = useParams<{ orderId: string }>();
 
   return (
-    <section className={styles["order-confirmation"]}>
+    <section className="pt-12 pb-12 sm:pt-14 sm:pb-14 lg:pt-16 lg:pb-16">
       <Container>
-        <h1 className={styles["order-confirmation__title"]}>
+        <h1 className="font-medium text-accent-foreground mb-2">
           ¡Muchas gracias por tu compra!
         </h1>
-        <p className={styles["order-confirmation__heading"]}>
+        <p className="text-2xl font-bold leading-9 mb-2">
           Tu orden está en camino
         </p>
-        <p className={styles["order-confirmation__description"]}>
+        <p className="text-muted-foreground mb-12">
           Llegaremos a la puerta de tu domicilio lo antes posible
         </p>
-        <p className={styles["order-confirmation__tracking-label"]}>
+        <p className="text-lg leading-5 font-medium mb-2">
           Código de seguimiento
         </p>
-        <p className={styles["order-confirmation__tracking-code"]}>{orderId}</p>
+        <p className="text-lg leading-5 font-medium text-accent-foreground">{orderId}</p>
       </Container>
     </section>
   );
