fc46bbd94dc996bdae396dc6191b1f1e5a756995
Jota <jsaldana999@gmail.com>
2025-04-02 19:55:12 -0500

    refactor: simplify Select component by removing variant support and inline styles

--- src/components/ui/select/index.tsx
+++ src/components/ui/select/index.tsx
@@ -1,40 +1,21 @@
 import * as React from "react";
 import { cn } from "@/lib/utils";
 
-import { cva, VariantProps } from "class-variance-authority";
-
-const selectVariants = cva(
-  "w-full rounded-xl border border-input bg-background px-3 py-2 text-sm",
-  {
-    variants: {
-      variant: {
-        default: "text-foreground",
-        disabled: "cursor-not-allowed opacity-50",
-      },
-    },
-    defaultVariants: {
-      variant: "default",
-    },
-  }
-);
-
-interface SelectProps
-  extends React.ComponentPropsWithoutRef<"select">,
-    VariantProps<typeof selectVariants> {
-  asChild?: boolean;
-}
-
-const Select = React.forwardRef<React.ElementRef<"select">, SelectProps>(
-  ({ className, variant, children, ...props }, ref) => (
-    <select
-      ref={ref}
-      className={cn(selectVariants({ variant }), className)}
-      {...props}
-    >
-      {children}
-    </select>
-  )
-);
+const Select = React.forwardRef<
+  HTMLSelectElement,
+  React.SelectHTMLAttributes<HTMLSelectElement>
+>(({ className, children, ...props }, ref) => (
+  <select
+    ref={ref}
+    className={cn(
+      "w-full rounded-xl border border-input bg-background px-3 py-2 text-sm disabled:cursor-not-allowed disabled:opacity-50",
+      className
+    )}
+    {...props}
+  >
+    {children}
+  </select>
+));
 Select.displayName = "BasicSelect";
 
 const Option = React.forwardRef<
