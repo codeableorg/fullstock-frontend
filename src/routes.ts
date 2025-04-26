b9a4eed6a695f28dbabdd25a5c9a94377fad61af
Angel <angelb.c@hotmail.com>
2025-04-25 20:00:06 -0500

    feat: implement resources routes modules using React Router v7 in Framework Mode

--- src/routes.ts
+++ src/routes.ts
@@ -8,14 +8,8 @@ export default [
       route("/:category", "./routes/category/index.tsx"),
       route("/products/:id", "./routes/product/index.tsx"),
       route("/cart", "./routes/cart/index.tsx"),
-      {
-        path: "/cart/add-item",
-        file: "./routes/cart/add-item/index.tsx",
-      },
-      {
-        path: "/cart/remove-item",
-        file: "./routes/cart/remove-item/index.tsx",
-      },
+      route("/cart/add-item", "./routes/cart/add-item/index.tsx"),
+      route("/cart/remove-item", "./routes/cart/remove-item/index.tsx"),
       route("/checkout", "./routes/checkout/index.tsx"),
       route(
         "/order-confirmation/:orderId",
@@ -23,10 +17,7 @@ export default [
       ),
       route("/login", "./routes/login/index.tsx"),
       route("/signup", "./routes/signup/index.tsx"),
-      {
-        path: "/logout",
-        file: "./routes/logout/index.tsx",
-      },
+      route("/logout", "./routes/logout/index.tsx"),
       route("account", "./routes/account/index.tsx", [
         route("profile", "./routes/account/profile/index.tsx"),
         route("orders", "./routes/account/orders/index.tsx"),
