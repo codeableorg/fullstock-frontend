9a51c9e64b09925c2988e83363a328370a54a7ec
Diego Torres <diegotc86@gmail.com>
2025-04-30 19:20:01 -0500

    todo: implement client.server.ts

---
+++ src/lib/client.server.ts
@@ -0,0 +1,46 @@
+import { API_URL } from "@/config";
+import { isApiError } from "@/models/error.model";
+import type { RequestConfig } from "@/models/request.model";
+
+export async function serverClient<T>(
+  endpoint: string,
+  // agregar el `request` como argumento
+  { body, headers: customHeaders, ...customConfig }: RequestConfig = {}
+) {
+  // Obtener el token desde las cookies
+  const token = getToken();
+
+  const config: RequestInit = {
+    method: body ? "POST" : "GET",
+    body: body ? JSON.stringify(body) : undefined,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: token ? `Bearer ${token}` : "",
+      ...customHeaders,
+    },
+    ...customConfig,
+  };
+
+  try {
+    const response = await fetch(API_URL + endpoint, config);
+    const data = await response.json();
+
+    if (response.ok) {
+      return data as T;
+    }
+
+    if (response.status === 401 && token) {
+      // En lugar de eliminar el token y redirigir, emitir un error customizado
+      // para el caller se encargue de manejar el caso de error de autenticación
+      removeToken();
+      // window.location.assign(window.location.pathname);
+      window.location.assign("/login");
+    }
+
+    if (isApiError(data)) throw new Error(data.error.message);
+    throw new Error("Unknown error");
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
