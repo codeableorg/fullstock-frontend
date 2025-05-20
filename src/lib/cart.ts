bb689b76c08941108520142994af39578cbea7df
Angel <angelb.c@hotmail.com>
2025-05-20 08:20:09 -0500

    refactor(cart): streamline addToCart and removeFromCart functions by removing unused imports and simplifying logic

--- src/lib/cart.ts
+++ src/lib/cart.ts
@@ -1,13 +1,10 @@
-import type { CartItem } from "@/models/cart.model";
+import type { Cart, CartItem } from "@/models/cart.model";
 import { type Product } from "@/models/product.model";
 import {
-  alterQuantityCartItem,
-  // createRemoteItems,
-  deleteRemoteCartItem,
-  getCurrentCart,
+  getCurrentCart
 } from "@/services/cart.server";
-import { getProductById } from "@/services/product.server";
 import { commitSession, getSession } from "@/session.server";
+import { serverClient } from "./client.server";
 
 export async function getCart(request: Request) {
   try {
@@ -19,21 +16,18 @@ export async function getCart(request: Request) {
     return null;
   }
 }
-
 export async function addToCart(
   productId: Product["id"],
   quantity: number = 1,
   request: Request
 ) {
   try {
-    const product = await getProductById(productId, request);
-    const updatedCart = await alterQuantityCartItem(
-      product.id,
-      quantity,
-      request
-    );
+    const updatedCart = await serverClient<Cart>("/cart/add-item", request, {
+      body: { productId, quantity },
+      includeCartSessionId: true,
+    });
 
-     if (updatedCart) {
+    if (updatedCart) {
       const session = await getSession(request.headers.get("Cookie"));
       session.set("cartSessionId", updatedCart.id);
       const committedSession = await commitSession(session);
@@ -48,19 +42,19 @@ export async function addToCart(
 
     return { cart: updatedCart };
   } catch (error) {
-    console.error(error);
+    console.error("Error adding to cart:", error);
     return null;
   }
 }
 
 export async function removeFromCart(itemId: CartItem["id"], request: Request) {
   try {
-    const updatedCart = await deleteRemoteCartItem( itemId, request);
-
-    return updatedCart;
-    //}
+    return serverClient<Cart>(`/cart/delete-item/${itemId}`, request, {
+      method: "DELETE",
+      includeCartSessionId: true,
+    });
   } catch (error) {
-    console.error(error);
+    console.error("Error removing from cart:", error);
     return null;
   }
 }
