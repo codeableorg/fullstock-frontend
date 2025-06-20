745e3ec4ba3ddc3c57d22ab384b0a1f98d0dceab
Diego Torres <diegotc86@gmail.com>
2025-06-19 20:56:10 -0500

    fix: add ON DELETE CASCADE to user_id reference in carts table and update secure cookie setting based on environment

--- src/e2e/demo.signin.spec.ts
+++ src/e2e/demo.signin.spec.ts
@@ -1,6 +1,38 @@
 import { test, expect } from "@playwright/test";
 
+import { hashPassword } from "@/lib/security";
+import type { CreateUserDTO } from "@/models/user.model";
+import {
+  createUser,
+  deleteUser,
+  getUserByEmail,
+} from "@/repositories/user.repository";
+
 test.describe("Visitante inicio sesion", () => {
+  let testUserId: number;
+
+  test.beforeAll(async () => {
+    const testUser: CreateUserDTO = {
+      email: "diego@codeable.com",
+      name: null,
+      password: await hashPassword("letmein"),
+      isGuest: false,
+    };
+
+    const existingUser = await getUserByEmail(testUser.email);
+
+    if (existingUser) {
+      await deleteUser(existingUser.id);
+    }
+
+    const user = await createUser(testUser);
+    testUserId = user.id;
+  });
+
+  test.afterAll(async () => {
+    await deleteUser(testUserId);
+  });
+
   test("test", async ({ page }) => {
     await page.goto("http://localhost:5173/");
     await page.getByTestId("login").click();
