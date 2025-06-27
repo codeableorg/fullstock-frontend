42e41055c1dec154040d96dcc202248d8522063d
Jota <jsaldana999@gmail.com>
2025-06-27 17:51:41 -0500

    fix: omit password field from AuthResponse user type

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -3,6 +3,6 @@ import type { User as PrismaUser } from "generated/prisma/client";
 export type User = PrismaUser;
 
 export interface AuthResponse {
-  user: User;
+  user: Omit<User, "password">;
   token: string;
 }
