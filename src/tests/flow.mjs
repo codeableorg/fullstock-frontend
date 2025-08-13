1f28f267e99620f6b74625fabd3bdf1d16a5cd36
Diego Torres <diegotc86@gmail.com>
2025-08-12 20:18:44 -0500

    Refactor test flow and configuration
    - Updated the import statement in flow.mjs to use double quotes for consistency.
    - Changed the base URL in test-users.yml to use an environment variable for better flexibility.
    - Adjusted the ramp duration and arrival rate in test-users.yml for load testing.
    - Removed the users.csv file as it is no longer needed for the tests.

--- src/tests/flow.mjs
+++ src/tests/flow.mjs
@@ -1,4 +1,4 @@
-import { expect } from '@playwright/test';
+import { expect } from "@playwright/test";
 
 const creditCards = {
   valid: {
@@ -27,69 +27,68 @@ const createOrderFormData = (overrides) => ({
 });
 
 export async function guestUserPurchase(page) {
+  // console.log(page);
   // Navegar a la tienda y agregar un producto
-      await page.goto("http://localhost:50404");
-  
-      await page.getByRole("menuitem", { name: "Polos" }).click();
-      await page.getByTestId("product-item").first().click();
-  
-      await page.getByRole("button", { name: "Agregar al Carrito" }).click();
-      await page.getByRole("link", { name: "Carrito de compras" }).click();
-  
-      await page.getByRole("link", { name: "Continuar Compra" }).click();
-  
-      // Llenar correctamente los campos
-      const orderForm = createOrderFormData();
-      for (const [key, value] of Object.entries(orderForm)) {
-        const input = await page.getByRole("textbox", { name: key });
-        await input.click();
-        await input.fill(value);
-      }
-      await page.getByRole("combobox", { name: "País" }).selectOption("PE");
-  
-      await page.getByRole("button", { name: "Confirmar Orden" }).click();
+  await page.goto(process.env.BASE_URL);
+
+  await page.getByRole("menuitem", { name: "Polos" }).click();
+  await page.getByTestId("product-item").first().click();
+
+  await page.getByRole("button", { name: "Agregar al Carrito" }).click();
+  await page.getByRole("link", { name: "Carrito de compras" }).click();
+
+  await page.getByRole("link", { name: "Continuar Compra" }).click();
+
+  // Llenar correctamente los campos
+  const orderForm = createOrderFormData();
+  for (const [key, value] of Object.entries(orderForm)) {
+    const input = await page.getByRole("textbox", { name: key });
+    await input.click();
+    await input.fill(value);
+  }
+  await page.getByRole("combobox", { name: "País" }).selectOption("PE");
+
+  await page.getByRole("button", { name: "Confirmar Orden" }).click();
   const checkoutFrame = page.locator('iframe[name="checkout_frame"]');
-      await expect(checkoutFrame).toBeVisible({ timeout: 10000 });
-  
-      const validCard = creditCards.valid;
-  
-      await checkoutFrame
-        .contentFrame()
-        .getByRole("textbox", { name: "#### #### #### ####" })
-        .fill(validCard.number);
-  
-      await expect(
-        checkoutFrame.contentFrame().getByRole("img", { name: "Culqi icon" })
-      ).toBeVisible();
-  
-      await checkoutFrame
-        .contentFrame()
-        .getByRole("textbox", { name: "MM/AA" })
-        .fill(validCard.exp);
-  
-      await checkoutFrame
-        .contentFrame()
-        .getByRole("textbox", { name: "CVV" })
-        .fill(validCard.cvv);
-  
-      await checkoutFrame
-        .contentFrame()
-        .getByRole("textbox", { name: "correo@electronico.com" })
-        .fill(orderForm["Correo electrónico"]);
-  
-      await expect(await checkoutFrame
-        .contentFrame()
-        .getByRole("button", { name: "Pagar S/" })
-      )
-        .toBeEnabled();
-      
-        await checkoutFrame
-        .contentFrame()
-        .getByRole("button", { name: "Pagar S/" })
-        .click();
-        
-      await expect(page.getByText("¡Muchas gracias por tu compra!")).toBeVisible({
-        timeout: 10000,
-      });
-      await expect(page.getByTestId("orderId")).toBeVisible();
+  await expect(checkoutFrame).toBeVisible({ timeout: 10000 });
+
+  const validCard = creditCards.valid;
+
+  await checkoutFrame
+    .contentFrame()
+    .getByRole("textbox", { name: "#### #### #### ####" })
+    .fill(validCard.number);
+
+  await expect(
+    checkoutFrame.contentFrame().getByRole("img", { name: "Culqi icon" })
+  ).toBeVisible();
+
+  await checkoutFrame
+    .contentFrame()
+    .getByRole("textbox", { name: "MM/AA" })
+    .fill(validCard.exp);
+
+  await checkoutFrame
+    .contentFrame()
+    .getByRole("textbox", { name: "CVV" })
+    .fill(validCard.cvv);
+
+  await checkoutFrame
+    .contentFrame()
+    .getByRole("textbox", { name: "correo@electronico.com" })
+    .fill(orderForm["Correo electrónico"]);
+
+  await expect(
+    await checkoutFrame.contentFrame().getByRole("button", { name: "Pagar S/" })
+  ).toBeEnabled();
+
+  await checkoutFrame
+    .contentFrame()
+    .getByRole("button", { name: "Pagar S/" })
+    .click();
+
+  await expect(page.getByText("¡Muchas gracias por tu compra!")).toBeVisible({
+    timeout: 10000,
+  });
+  await expect(page.getByTestId("orderId")).toBeVisible();
 }
