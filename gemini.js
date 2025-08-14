c4d18c0d3eae5cc4669e6e5d2f82df5b7a5f8025
Diego Torres <diegotc86@gmail.com>
2025-08-13 19:57:51 -0500

    feat: implement chat functionality with Google GenAI

--- gemini.js
+++ gemini.js
@@ -8,12 +8,31 @@ const ai = new GoogleGenAI({
 });
 
 async function main() {
-  const response = await ai.models.generateContent({
+  const chat = ai.chats.create({
     model: "gemini-2.5-flash",
-    contents: "Explica cómo funciona la IA en pocas palabras",
+    history: [],
   });
 
-  console.log(response.text);
+  const message1 = "Hola, mi nombre es Diego";
+
+  console.log("User: ", message1);
+
+  const response1 = await chat.sendMessageStream({
+    message: message1,
+  });
+  for await (const chunk of response1) {
+    console.log(chunk.text);
+  }
+
+  const message2 = "Sabes cuál es mi nombre?";
+  console.log("User: ", message2);
+
+  const response2 = await chat.sendMessageStream({
+    message: message2,
+  });
+  for await (const chunk of response2) {
+    console.log(chunk.text);
+  }
 }
 
 await main();
