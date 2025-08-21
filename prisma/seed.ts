cba1cee320a697f7a1064c3bd15581abff99b10d
Jota <jsaldana999@gmail.com>
2025-08-21 11:11:19 -0500

    se agregan modificaciones a la base de datos para soportar el nuevo requerimiento

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,18 +1,41 @@
-import { categories, products } from "./initial_data";
+import { categories, categoryVariants, products } from "./initial_data";
 import { PrismaClient } from "../generated/prisma/client";
 
 const prisma = new PrismaClient();
 
 async function seedDb() {
+  await prisma.orderItem.deleteMany();
+  console.log("  ✅ OrderItems eliminados");
+
+  await prisma.cart.deleteMany();
+  console.log("  ✅ Carts eliminados");
+
+  await prisma.cartItem.deleteMany();
+  console.log("  ✅ CartItems eliminados");
+
+  await prisma.categoryVariant.deleteMany();
+  console.log("  ✅ CategoryVariants eliminados");
+
+  await prisma.product.deleteMany();
+  console.log("  ✅ Products eliminados");
+
+  await prisma.category.deleteMany();
+  console.log("  ✅ Categories eliminadas");
+
   await prisma.category.createMany({
     data: categories,
   });
   console.log("1. Categories successfully inserted");
 
+  await prisma.categoryVariant.createMany({
+    data: categoryVariants,
+  });
+  console.log("2. Category variants successfully inserted");
+
   await prisma.product.createMany({
     data: products,
   });
-  console.log("2. Products successfully inserted");
+  console.log("3. Products successfully inserted");
 }
 
 seedDb()
