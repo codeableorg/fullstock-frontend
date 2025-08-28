b09d189d36989f2eaa37917537e5e38f9760fb6a
Kellyarias02 <kellyarias@gmail.com>
2025-08-27 20:23:46 -0500

    Refactor: to add interactivity hover category stickers and to improve the user experiencie.

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -4,19 +4,6 @@ import type { Product } from "@/models/product.model";
 
 import { getCategoryBySlug } from "./category.service";
 
-export async function getProductsByCategorySlug(
-  categorySlug: Category["slug"]
-): Promise<Product[]> {
-  const category = await getCategoryBySlug(categorySlug);
-  const products = await prisma.product.findMany({
-    where: { categoryId: category.id },
-  });
-
-  return products.map((product) => ({
-    ...product,
-    price: product.price.toNumber(),
-  }));
-}
 
 export async function getProductById(id: number): Promise<Product | null> {
   const product = await prisma.product.findUnique({
@@ -46,5 +33,32 @@ export async function getAllProducts(): Promise<Product[]> {
   return (await prisma.product.findMany()).map((p) => ({
     ...p,
     price: p.price.toNumber(),
+  }));
+}
+
+export async function getProductsByCategorySlug(
+  categorySlug: Category["slug"]
+): Promise<Product[]> {
+  const category = await getCategoryBySlug(categorySlug);
+  const products = await prisma.product.findMany({
+    where: { categoryId: category.id },
+    include: {
+      stickersVariants: true,
+      variants: true
+    },
+  });
+
+  return products.map((product) => ({
+    ...product,
+    price: product.price.toNumber(),
+    variants: product.variants?.map(v => ({
+      id: v.id,
+      size: v.size as "small" | "medium" | "large",
+    })),
+    stickersVariants: product.stickersVariants?.map(s => ({
+      id: s.id,
+      measure: s.measure as "3*3" | "5*5" | "10*10",
+      price: s.price.toNumber(),
+    })),
   }));
 }
