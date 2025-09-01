e7ad0bfac8206a5ee05408fa3c8f0bb6a7a08535
mike <mike.vera.q@gmail.com>
2025-09-01 02:30:04 -0500

    feat: update data models and cart logic to support variant attribute values in orders and cart items

--- src/routes/cart/index.tsx
+++ src/routes/cart/index.tsx
@@ -30,65 +30,71 @@ export default function Cart({ loaderData }: Route.ComponentProps) {
           Carrito de compras
         </h1>
         <div className="border-solid border rounded-xl flex flex-col">
-          {cart?.items?.map(({ product, quantity, id, variantAttributeValue
- }) => (
-            <div key={variantAttributeValue?.id} className="flex gap-7 p-6 border-b">
-              <div className="w-20 rounded-xl bg-muted">
-                <img
-                  src={product.imgSrc}
-                  alt={product.alt || product.title}
-                  className="w-full aspect-[2/3] object-contain"
-                />
-              </div>
-              <div className="flex grow flex-col justify-between">
-                <div className="flex gap-4 justify-between items-center">
-                  <h2 className="text-sm">{product.title} ({variantAttributeValue?.value})</h2>
-                  <Form method="post" action="/cart/remove-item">
-                    <Button
-                      size="sm-icon"
-                      variant="outline"
-                      name="itemId"
-                      value={id}
-                    >
-                      <Trash2 />
-                    </Button>
-                  </Form>
+          {cart?.items?.map(
+            ({ product, quantity, id, variantAttributeValue }) => (
+              <div
+                key={variantAttributeValue?.id}
+                className="flex gap-7 p-6 border-b"
+              >
+                <div className="w-20 rounded-xl bg-muted">
+                  <img
+                    src={product.imgSrc}
+                    alt={product.alt || product.title}
+                    className="w-full aspect-[2/3] object-contain"
+                  />
                 </div>
-                <div className="flex flex-col gap-2 md:flex-row md:justify-between md:items-center">
-                  <p className="text-sm font-medium">
-                    ${product.price!.toFixed(2)}
-                  </p>
-                  <div className="flex gap-4 items-center">
-                    <Form method="post" action="/cart/add-item">
-                      <input type="hidden" name="quantity" value="-1" />
+                <div className="flex grow flex-col justify-between">
+                  <div className="flex gap-4 justify-between items-center">
+                    <h2 className="text-sm">
+                      {product.title} ({variantAttributeValue?.value})
+                    </h2>
+                    <Form method="post" action="/cart/remove-item">
                       <Button
-                        name="attributeValueId"
-                        value={variantAttributeValue?.id}
-                        variant="outline"
                         size="sm-icon"
-                        disabled={quantity === 1}
-                      >
-                        <Minus />
-                      </Button>
-                    </Form>
-                    <span className="h-8 w-8 flex justify-center items-center border rounded-md py-2 px-4">
-                      {quantity}
-                    </span>
-                    <Form method="post" action="/cart/add-item">
-                      <Button
                         variant="outline"
-                        size="sm-icon"
-                        name="attributeValueId"
-                        value={variantAttributeValue?.id}
+                        name="itemId"
+                        value={id}
                       >
-                        <Plus />
+                        <Trash2 />
                       </Button>
                     </Form>
                   </div>
+                  <div className="flex flex-col gap-2 md:flex-row md:justify-between md:items-center">
+                    <p className="text-sm font-medium">
+                      ${product.price!.toFixed(2)}
+                    </p>
+                    <div className="flex gap-4 items-center">
+                      <Form method="post" action="/cart/add-item">
+                        <input type="hidden" name="quantity" value="-1" />
+                        <Button
+                          name="attributeValueId"
+                          value={variantAttributeValue?.id}
+                          variant="outline"
+                          size="sm-icon"
+                          disabled={quantity === 1}
+                        >
+                          <Minus />
+                        </Button>
+                      </Form>
+                      <span className="h-8 w-8 flex justify-center items-center border rounded-md py-2 px-4">
+                        {quantity}
+                      </span>
+                      <Form method="post" action="/cart/add-item">
+                        <Button
+                          variant="outline"
+                          size="sm-icon"
+                          name="attributeValueId"
+                          value={variantAttributeValue?.id}
+                        >
+                          <Plus />
+                        </Button>
+                      </Form>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            )
+          )}
           <div className="flex justify-between p-6 text-base font-medium border-b">
             <p>Total</p>
             <p>S/{total.toFixed(2)}</p>
@@ -107,3 +113,4 @@ export default function Cart({ loaderData }: Route.ComponentProps) {
     </Section>
   );
 }
+
